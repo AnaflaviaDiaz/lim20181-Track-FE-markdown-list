277b7e12b6eb76a708b14bc96ccc432b8c445c28
Anaflavia <anaflaviadiazmartel5a@gmail.com>
2018-08-12 23:31:06 -0500

    closes #10

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 'use strict';
 
-exports.printMsg = function () {
-	console.log("This is a message from the demo package");
-}
+console.log("Entré")
 
-function sum(a, b) {
-  return a + b;
-}
-module.exports = sum;
+// exports.printMsg = function () {
+// 	console.log("This is a message from the demo package");
+// }
+
+// function sum(a, b) {
+//   return a + b;
+// }
+// module.exports = sum;
