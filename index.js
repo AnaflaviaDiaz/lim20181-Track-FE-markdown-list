8f2ac1167e1d74fb7384b08592fa8acfc193d4f8
Anaflavia <anaflaviadiazmartel5a@gmail.com>
2018-08-15 10:06:36 -0500

    update #1

--- index.js
+++ index.js
@@ -1,12 +1,59 @@
+#!/usr/bin/env node
+
 'use strict';
 
-console.log("Entré")
+const fs = require('fs'),
+	[, , ...args] = process.argv /* interpreta comandos en cli*/,
+	toAbsolutePath = require('to-absolute-path');
 
-// exports.printMsg = function () {
-// 	console.log("This is a message from the demo package");
+// diferenciación de carpeta y archivo
+const directoryFile = () => {
+	// convierte ruta relativa en absoluta
+	const route = toAbsolutePath(args[0]);
+
+	fs.stat(route, (err, stats) => {
+		if (stats.isDirectory()) {
+			console.log(stats.isDirectory());
+		}
+		if (stats.isFile()) {
+			console.log('es archivo');
+		}
+	});
+};
+
+directoryFile();
+
+// ruta de la carpeta
+// const mdLinks = () => {
+// 	console.log(`La ruta es ${args[0]}`);
+// };
+
+// mdLinks();
+
+// validacion de opciones en comando
+// if (args[1] === '--stats' && args[2] === '--validate') {
+// 	console.log(args[1], args[2]);
+// } else if (args[1] === '--stats' && args[2] === undefined) {
+// 	console.log(`la opcion es ${args[1]}`);
+// } else if (args[1] === '--validate' && args[2] === undefined) {
+// 	console.log(`la opcion es ${args[1]}`);
 // }
 
-// function sum(a, b) {
-//   return a + b;
-// }
-// module.exports = sum;
+// lee archivos
+// fs.readFile('prueba/README.md', 'utf8', (err, data) => {
+// 	if (err) throw err;
+// 	console.log(data);
+// });
+
+// lee directorios
+// fs.readdir('prueba', 'utf8', (err, data) => {
+// 	if (err) throw err;
+// 	console.log(data);
+// });
+
+// fs.readdir('prueba', 'utf8', (err, data) => {
+// 	if (err) throw err;
+// 	data.map(contenido => {
+// 		console.log(path.extname(contenido));
+// 	});
+// });
